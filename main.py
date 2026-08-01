b202716e5ef4c1e07d389d6628ea7c47f25dcc26
Daniel Karczewicz <dkarczewicz.2000@gmail.com>
2026-08-01 12:49:52 +0200

    splat script into functions

--- main.py
+++ main.py
@@ -5,33 +5,51 @@ import re
 import pdftotext
 
 
-def read_document(filename: str) -> list[str]:
-	pass
-
-
-def split_data(data_array: list[str]) -> dict:
-	pass
-
-
-def process_date(date: str):
-	pass
-
-
-def generate_summary():
-	pass
-
-
-def main():
-
-	with open("invoices/invoice_Frank Carlisle_49474.pdf", "rb") as invoice_file:
+def read_document_to_pdf(filename: str) -> str:
+	with open(filename, "rb") as invoice_file:
 		invoice_text = pdftotext.PDF(invoice_file)
+
+	return invoice_text
+
+def split_data(invoice_text: str) -> dict:
 	invoice_number = re.search("[#]\\s\\d{5}", invoice_text[0])
 	invoice_date = re.search("\\D{3}\\s\\d{2}\\s\\d{4}", invoice_text[0])
 	invoice_total_amount = re.search("Total:\\s+\\$([\\d]+,[\\d]+\\.\\d{2})", invoice_text[0])
+	return {
+		"invoice_number" : invoice_number.group().replace(" ", ""),
+		"invoice_date" : process_date(invoice_date.group()),
+		"invoice_total_amount" : invoice_total_amount.group().split("$")[-1].replace(",", "")
+	}
 
-	print(invoice_number.group())
-	print(invoice_date.group())
-	print(invoice_total_amount.group())
+
+def process_date(date: str) -> str:
+	[month, day, year] = date.split(" ")
+	month_names = {
+		"Jan" : "01",
+		"Feb" : "02",
+		"Mar" : "03",
+		"Apr" : "04",
+		"May" : "05",
+		"Jun" : "06",
+		"Jul" : "07",
+		"Aug" : "08",
+		"Spe" : "09",
+		"Oct" : "10",
+		"Nov" : "11",
+		"Dec" : "12",
+	}
+	return f"{day}.{month_names[month]}.{year}"
+
+
+def generate_summary(invoice_data: dict) -> str:
+	print(f"{invoice_data['invoice_number']},{invoice_data['invoice_date']},{invoice_data['invoice_total_amount']}")
+	
+
+
+def main():
+	invoice_text = read_document_to_pdf("invoices/invoice_Frank Carlisle_49474.pdf")
+	invoice_data = split_data(invoice_text)
+	generate_summary(invoice_data)
 
 
 if __name__ == '__main__':
