9534981c15e3c1ead9fba36b228943ac3f53a9cf
Daniel Karczewicz <dkarczewicz.2000@gmail.com>
2026-08-01 21:20:26 +0200

    script is processing invoices in loop, dirty fix was created to omit errors related to regex (for instance due to different date formats or amounts)

--- main.py
+++ main.py
@@ -12,13 +12,28 @@ def read_document_to_pdf(filename: str) -> str:
 	return invoice_text
 
 def split_data(invoice_text: str) -> dict:
-	invoice_number = re.search("[#]\\s\\d{5}", invoice_text[0])
+	invoice_number = re.search("[#]\\s\\d{4,5}", invoice_text[0])
 	invoice_date = re.search("\\D{3}\\s\\d{2}\\s\\d{4}", invoice_text[0])
-	invoice_total_amount = re.search("Total:\\s+\\$([\\d]+,[\\d]+\\.\\d{2})", invoice_text[0])
+	invoice_total_amount = re.search("Total:\\s+\\$([\\d]*,?[\\d]+\\.\\d{2})", invoice_text[0])
+	try:
+		invoice_number = invoice_number.group().replace(" ", "")
+	except AttributeError:
+		invoice_number = ""
+
+	try:
+		invoice_date = process_date(invoice_date.group())
+	except AttributeError:
+		invoice_date = ""
+
+	try:
+		invoice_total_amount = invoice_total_amount.group().split("$")[-1].replace(",", "")
+	except AttributeError:
+		invoice_total_amount = ""
+	
 	return {
-		"invoice_number" : invoice_number.group().replace(" ", ""),
-		"invoice_date" : process_date(invoice_date.group()),
-		"invoice_total_amount" : invoice_total_amount.group().split("$")[-1].replace(",", "")
+		"invoice_number" : invoice_number,
+		"invoice_date" : invoice_date,
+		"invoice_total_amount" : invoice_total_amount 
 	}
 
 
@@ -33,7 +48,7 @@ def process_date(date: str) -> str:
 		"Jun" : "06",
 		"Jul" : "07",
 		"Aug" : "08",
-		"Spe" : "09",
+		"Sep" : "09",
 		"Oct" : "10",
 		"Nov" : "11",
 		"Dec" : "12",
@@ -46,15 +61,17 @@ def generate_invoice_summary(invoice_data: dict) -> str:
 
 
 def generate_csv_summary():
-	for invoice_file in os.listdir("./invoices"):
-		invoice_text = read_document_to_pdf(f"./invoices/{invoice_file}")
-		invoice_data = split_data(invoice_text)
-		print(generate_invoice_summary(invoice_data))
+	with open("output.csv", "w") as invoice_summary_file:
+		invoice_summary_file.write("invoice number, invoice data, invoice total amount\n")
+		for invoice_file in os.listdir("./invoices"):
+			invoice_text = read_document_to_pdf(f"./invoices/{invoice_file}")
+			invoice_data = split_data(invoice_text)
+			invoice_summary_file.write(f"{generate_invoice_summary(invoice_data)}\n")
 
 
 def main():
 	# invoice_text = read_document_to_pdf("invoices/invoice_Frank Carlisle_49474.pdf")
-	generate_csv_summary()	
+	generate_csv_summary()
 
 
 if __name__ == '__main__':
